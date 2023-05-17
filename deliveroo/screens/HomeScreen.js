10d245ba833e609b19e1fabb54293f63c3b2421f
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-05-17 19:15:14 +0300

    feat: header design

--- deliveroo/screens/HomeScreen.js
+++ deliveroo/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronDown, User2Icon, SearchIcon } from "lucide-react-native";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -11,18 +12,21 @@ const HomeScreen = () => {
     });
   }, []);
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{
-            url: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
+            uri: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-          <Text className="font-bold text-xl">Current Location</Text>
+          <Text className="font-bold text-xl">Current Location
+          <ChevronDown size={20} color="#00CCBB"/>
+          </Text>
         </View>
+        <User2Icon size={25} color="#00CCBB"/>
       </View>
     </SafeAreaView>
   );
